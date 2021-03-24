208ab9aeaff2deb61a8a42c9ebcba782387bfd61
Muslim Razi <razi.muslim@gmail.com>
2021-03-24 11:35:29 -0700

    update DEEPDRIVE_LABELS to support bdd labels 2020

--- deepdrive_dataset/deepdrive_versions.py
+++ deepdrive_dataset/deepdrive_versions.py
@@ -1,3 +1,3 @@
 DEEPDRIVE_FOLDS = ['train', 'val', 'test']
 DEEPDRIVE_VERSIONS = ['100k', '10k']
-DEEPDRIVE_LABELS = ['bus', 'traffic light', 'traffic sign', 'person', 'bike', 'truck', 'motor', 'car', 'train', 'rider']
+DEEPDRIVE_LABELS = ['bus', 'traffic light', 'traffic sign', 'person', 'bike', 'truck', 'motor', 'car', 'train', 'rider', 'pedestrian', 'other vehicle', 'bicycle', 'motorcycle', 'trailer', 'other person']
